86cccb4f478d863c1400aa087e34d61cca94b943
Muhammed Mifthah <107195547+mifthah123@users.noreply.github.com>
2024-03-31 23:54:59 +0530

    fixed bug on the authentication flow

--- src/helpers/login.js
+++ src/helpers/login.js
@@ -1,14 +1,14 @@
 export const login = (data, email, password, navigate, state) => {
-  for (let val of data) {
+  console.log(data);
+  for (const val of data) {
+    console.log(val);
     if (val.email === email) {
       if (val.Password === password) {
         state(true);
-        
       } else {
         return "Your password is incorrect.";
       }
-    } else {
-      return "We couldn't find your email id.";
     }
   }
+  return "We couldn't find your email id.";
 };
